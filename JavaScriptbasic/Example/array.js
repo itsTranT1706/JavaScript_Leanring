c19588312866c4d0cf78ad7c519c79edf3c8de27
itsTranT1706 <khiemtrangia60@gmail.com>
2023-02-20 01:02:26 +0700

    update lesson Js

---
+++ JavaScriptbasic/Example/array.js
@@ -0,0 +1,42 @@
+let car = ["mec", "toyota", "huyndai", ""];
+person = [
+    "Tien",
+    "Tung",
+    "bot",
+]
+//------------- create a array
+const home = [];
+home[0] = "chungcu";
+home[1] = "homestay";
+
+// console.log(typeof(car));
+// console.log(car[1]);
+// console.log(`hello world`);
+
+console.log(`who is the best? it's ${person[0]}`);
+console.log(home);
+let c = [
+    {
+        name: "Tien",
+        age: " 19",
+        gender: "male",
+
+    },
+    {
+        name: "Tung",
+        age: " 24",
+        gender: "male",
+
+    }
+];
+// console.log(`who is the best ? it's ${c[0].name}`);
+
+//------Js_comparision (so sánh)
+let a=5;
+let b="5";
+console.log(`compare  a, b with ==: a = ${a} and b = ${b}\nresult: ${a==b}` );
+console.log(`compare  a, b with === : a = ${a} and b = ${b}\nresult: ${a===b}` );
+//------------empty,null,undefined
+let tam={ };
+console.log(`check value : ${tam}`);
+
