2ba168779d619b44dd267b159cdb4ec450192558
itsTranT1706 <khiemtrangia60@gmail.com>
2023-07-30 01:21:57 +0700

    update

--- Example/array.js
+++ Example/array.js
@@ -310,4 +310,10 @@
 // console.log(tag);
 let elementNode = document.querySelector(".heading");
 elementNode.innerText = "F8 - Học lập trình để đi làm";
-elementNode.innerText = "F8 - Học lập trình để đi làm";
+document.write(`<h2></h2>`);
+let elementNode1= document.querySelector("h2");
+elementNode1.setAttribute("class","headingName");
+// elementNode1.textContent = "F8 - Học lập trình để đi làm";
+elementNode.setAttribute("style","color:blue");
+let boxElement = Document.querySelector(".box");
+console.log(boxElement);
