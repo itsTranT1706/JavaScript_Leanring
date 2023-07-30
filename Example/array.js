5c474072fa5e88ac5f3d2c963a9d82d14265944e
itsTranT1706 <khiemtrangia60@gmail.com>
2023-07-31 01:27:22 +0700

    innerHTML

--- Example/array.js
+++ Example/array.js
@@ -7,5 +7,5 @@ elementNode1.setAttribute("class","headingName");
 elementNode1.textContent = "F8 - Học lập trình để đi làm";
 elementNode.setAttribute("style","color:blue");
 let boxElement = document.querySelector(".tien");
-boxElement.innerHTML = "<p>Hello i am Tran Viet Tien</p>"
+boxElement.innerHTML = `<p class="tung" style ="color:red">Hello i am Tran Viet Tien</p>`
 console.log(boxElement);
