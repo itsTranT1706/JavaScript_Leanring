41e3f6d43f8d792c69b340f596cec0839c135fc4
itsTranT1706 <khiemtrangia60@gmail.com>
2023-07-31 00:59:01 +0700

    error

--- Example/array.js
+++ Example/array.js
@@ -1,9 +1,11 @@
 let elementNode = document.querySelector(".heading");
+console.log(elementNode)
 elementNode.innerText = "F8 - Học lập trình để đi làm";
 document.write(`<h2></h2>`);
 let elementNode1= document.querySelector("h2");
 elementNode1.setAttribute("class","headingName");
-// elementNode1.textContent = "F8 - Học lập trình để đi làm";
+elementNode1.textContent = "F8 - Học lập trình để đi làm";
 elementNode.setAttribute("style","color:blue");
-let boxElement = document.querySelector(".box");
+let boxElement = document.querySelector(".tien");
+// boxElement.innerHTML = "<p>Hello i am Tran Viet Tien</p>"
 console.log(boxElement);
