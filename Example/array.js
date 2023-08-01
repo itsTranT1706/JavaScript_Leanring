003117a8aa1e3bef864bcd5fe2c6b19c65646214
itsTranT1706 <khiemtrangia60@gmail.com>
2023-08-01 16:06:18 +0700

    modified: using object.assign()

--- Example/array.js
+++ Example/array.js
@@ -1,11 +1,20 @@
-// //solution1:
-// let nodeRed = document.querySelector(".red");
-// let nodeP = document.querySelector("p");
-// nodeP.setAttribute("style","color:#f05123");
-// nodeRed.setAttribute("style","background-color:#f00");
+// // //solution1:
+// // let nodeRed = document.querySelector(".red");
+// // let nodeP = document.querySelector("p");
+// // nodeP.setAttribute("style","color:#f05123");
+// // nodeRed.setAttribute("style","background-color:#f00");
 
-//solution 2:
-let nodeRed = document.querySelector(".red");
-nodeRed.style.backgroundColor = "#f00";
-let nodeP = document.querySelector("p");
-nodeP.style.color="#f05123";
+// //solution 2:
+// let nodeRed = document.querySelector(".red");
+// nodeRed.style.backgroundColor = "#f00";
+// let nodeP = document.querySelector("p");
+// nodeP.style.color="#f05123";
+
+//solution 3:
+Object.assign(document.querySelector(".red").style, {
+    backgroundColor :"#f00",
+
+})
+Object.assign(document.querySelector("p").style, {
+    color:"#f05123",
+})
