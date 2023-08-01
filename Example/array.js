6a429b34dc46e725c75090ea84876163061cd2c4
itsTranT1706 <khiemtrangia60@gmail.com>
2023-08-01 16:40:10 +0700

    classList property

--- Example/array.js
+++ Example/array.js
@@ -1,20 +1,40 @@
-// // //solution1:
+// // // //solution1:
+// // // let nodeRed = document.querySelector(".red");
+// // // let nodeP = document.querySelector("p");
+// // // nodeP.setAttribute("style","color:#f05123");
+// // // nodeRed.setAttribute("style","background-color:#f00");
+
+// // //solution 2:
 // // let nodeRed = document.querySelector(".red");
+// // nodeRed.style.backgroundColor = "#f00";
 // // let nodeP = document.querySelector("p");
-// // nodeP.setAttribute("style","color:#f05123");
-// // nodeRed.setAttribute("style","background-color:#f00");
+// // nodeP.style.color="#f05123";
 
-// //solution 2:
-// let nodeRed = document.querySelector(".red");
-// nodeRed.style.backgroundColor = "#f00";
-// let nodeP = document.querySelector("p");
-// nodeP.style.color="#f05123";
+// //solution 3:
+// Object.assign(document.querySelector(".red").style, {
+//     backgroundColor :"#f00",
 
-//solution 3:
-Object.assign(document.querySelector(".red").style, {
-    backgroundColor :"#f00",
-
+// })
+// Object.assign(document.querySelector("p").style, {
+//     color:"#f05123",
+// })
+let nodeHead = document.querySelector(".head");
+console.log([nodeHead.classList])
+nodeHead.classList.add("color");
+console.log(nodeHead.classList.contains("color"));
+// console.log(nodeHead.classList.remove("color"));
+// setTimeout(() => {
+//     nodeHead.classList.remove("color");
+// }, 5000);
+let button = document.querySelector(".button");
+Object.assign(button.style, {
+    width:"100px",
+    height:"100px",
+    backgroundColor:"blue",
 })
-Object.assign(document.querySelector("p").style, {
-    color:"#f05123",
-})
+// setInterval(()=>{
+//     button.classList.toggle("color")
+// },1000)
+setInterval(()=> { 
+    nodeHead.classList.toggle("color")
+}, 1000)
