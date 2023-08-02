49ce248cf5c77564a5c6fd2edc4381608e0cfe00
itsTranT1706 <khiemtrangia60@gmail.com>
2023-08-03 00:52:56 +0700

    format code

--- Example/array.js
+++ Example/array.js
@@ -1,25 +1,25 @@
 document.write(`<h1></h1>`);
-let nodeTag= document.querySelector("h1");
-nodeTag.setAttribute("class","heading");
+let nodeTag = document.querySelector("h1");
+nodeTag.setAttribute("class", "heading");
 let nodeHeading = document.querySelector(".heading");
 Object.assign(nodeHeading.style, {
-    width :"100px",
-    height :"100px",
-    color:"white",
+    width: "100px",
+    height: "100px",
+    color: "white",
 })
-nodeHeading.innerHTML="tran viet tien";
+nodeHeading.innerHTML = "tran viet tien";
 
 nodeHeading.classList.add("pink");
-let i=0;
-setInterval(()=> {
-            if (i<10) {
-                nodeHeading.classList.toggle("black");
-                i++;
-            }
-            else {
-                clearInterval();
-            }
-            
-},500)
+let i = 0;
+setInterval(() => {
+    if (i < 10) {
+        nodeHeading.classList.toggle("black");
+        i++;
+    }
+    else {
+        clearInterval();
+    }
+
+}, 500)
 
 console.log(nodeHeading.style)
