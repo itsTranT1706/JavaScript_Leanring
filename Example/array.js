8bcb994bfae4f461ef88e9531741a44f85ae3ac6
itsTranT1706 <khiemtrangia60@gmail.com>
2023-08-20 16:32:16 +0700

    command explain

--- Example/array.js
+++ Example/array.js
@@ -4,11 +4,11 @@ divElement.onclick = function(e) {
 }
 let buttonElement = document.querySelector("button");
 buttonElement.onclick = function(e) {
-      e.stopPropagation();
+      e.stopPropagation();//stopPropagation() stops an event from bubbling or propagating up the DOM tree.
       console.log("click me!");
 
 }
 let ulElement = document.querySelector("ul");
 ulElement.onmousedown = function(e) {
-      e.preventDefault();
+      e.preventDefault();//preventDefault() prevents the default browser behavior for a given element.
 }
