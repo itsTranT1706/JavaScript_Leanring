6b5324273c7612af314b1c0e4c69c7ce66a1c5bd
itsTranT1706 <khiemtrangia60@gmail.com>
2023-08-20 12:28:46 +0700

    click ul => show value on input

--- Example/array.js
+++ Example/array.js
@@ -1,7 +1,11 @@
 let ulElement = document.querySelector("ul");
 ulElement.onmousedown = function (e) {
-     e.preventDefault();
+      e.preventDefault();
 }
-ulElement.onclick = function(e) {
-      console.log(e.target)
+ulElement.onclick = function (e) {
+      console.log(e.target.innerHTML)
+}
+let input = document.querySelector(`input[type="text"]`);
+ulElement.onclick = function (e) {
+      input.value = e.target.innerHTML;
 }
