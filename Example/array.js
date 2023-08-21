fc3598269cebb0f083e421a18a9ace3f1c925b77
itsTranT1706 <khiemtrangia60@gmail.com>
2023-08-21 13:39:56 +0700

    loop click to change color

--- Example/array.js
+++ Example/array.js
@@ -1,23 +1,32 @@
-/**Bài tập: Cho trước thẻ button, hãy viết code JS sao cho mỗi lần click vào button 
- * sẽ đổi màu chữ của button lần lượt sang màu red, blue, green và sau 3 giây chỉ
- *  đổi màu red và blue (bỏ màu green) */
-let buttonElement = document.querySelector("button");
-function red() {
-      buttonElement.classList.toggle("red");
-}
-function blue() {
-      buttonElement.classList.toggle("blue");
-}
-function green() {
-      buttonElement.classList.toggle("green");
-}
-buttonElement.addEventListener("click",(e)=> {
-      red();
-      // e.stopPropagation();
-      buttonElement.addEventListener("click",()=> {
-            blue();
-            buttonElement.addEventListener("click",green);
-      });
-})
-// setTimeout(()=> {
-// },3000)
+// /**Bài tập: Cho trước thẻ button, hãy viết code JS sao cho mỗi lần click vào button 
+//  * sẽ đổi màu chữ của button lần lượt sang màu red, blue, green và sau 3 giây chỉ
+//  *  đổi màu red và blue (bỏ màu green) */
+// let buttonElement = document.querySelector("button");
+// function red() {
+//       buttonElement.classList.toggle("red");
+// }
+// function blue() {
+//       buttonElement.classList.toggle("blue");
+// }
+// function green() {
+//       buttonElement.classList.toggle("green");
+// }
+// buttonElement.addEventListener("click",()=> {
+//       red();
+//       // e.stopPropagation();
+//       buttonElement.addEventListener("click",()=> {
+//             blue();
+//             buttonElement.addEventListener("click",green);
+//       });
+// })
+// // setTimeout(()=> {
+// // },3000)
+let button = document.querySelector("button")
+    colors = ['purple', 'yellow', 'orange', 'brown', 'black'];
+
+button.onclick = function () {
+    color = colors.shift();
+    colors.push(color);
+
+    button.style.backgroundColor = color;
+};
