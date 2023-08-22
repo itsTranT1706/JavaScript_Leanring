f03e3f49cea1f54dcbfd74ed073b45270f9f65d6
itsTranT1706 <khiemtrangia60@gmail.com>
2023-08-23 01:41:05 +0700

    example about Promise

--- Example/main.js
+++ Example/main.js
@@ -1,17 +1,55 @@
-// //Promise?
-// setTimeout(()=> {
-//         console.log("1");
-// },3000);
-// console.log("2");
+// // //Promise?
+// // setTimeout(()=> {
+// //         console.log("1");
+// // },3000);
+// // console.log("2");
 
- //set up Promise 
- //1.new Promise
- //2.Executor
- let promise = new Promise(
-        // Executor
-        function(resolve, reject) {
-                //logic
-                //success : resolve()
-                //failure : reject()
-        }
- )
+//  //set up Promise 
+//  //1.new Promise
+//  //2.Executor
+//  let promise = new Promise(
+//         // Executor
+//         function(resolve, reject) {
+//                 //logic
+//                 //success : resolve()
+//                 //failure : reject()
+//                 resolve([
+//                         {
+//                                 id : 1,
+//                                 name:`tran viet tien`
+//                         }
+//                 ]);
+//         }
+//  )
+//  promise 
+//     .then(fucntion(person) {   // resolve() is called
+//                 console.log(person);
+//     } )
+//     .catch(fucntion() {  //reject()     is called
+//                 console.log("failure")
+//     } )
+//     .finally(fucntion() { 
+//                 console.log("done")
+//     } )
+
+new Promise(function(resolve, reject) {
+
+        setTimeout(() => resolve('em yêu anh'), 5000); // (*)
+        
+        }).then(function(result) { // (**)
+        
+        alert(result); // em yêu anh
+        return 'kiss thôi...';
+        
+        }).then(function(result) { // (***)
+        
+        alert(result); // kiss thôi
+        return 'xxx thôi';
+        
+        }).then(function(result) {
+        
+        alert(result); // xxx thôi 
+        return 'oh yea baby'; //kakakakak
+        
+        })
+        
