b8c547bfa3030e8f6a1cee1f7adbead9f7e0ce79
itsTranT1706 <khiemtrangia60@gmail.com>
2023-08-30 11:06:58 +0700

    promise practice

--- Example/main.js
+++ Example/main.js
@@ -1,40 +1,44 @@
+// /**:Bài toán: sau 1 giây log ra 1,2,3,4 */
 
-function hell(value, cb) {
-    cb(value);
+// //callbackHell
+// console.log("--------callback-------");
+// function sleep(ms) {
+//     setTimeout(() => {
+//         console.log(1);
+//         setTimeout(() => {
+//             console.log(2);
+//             setTimeout(() => {
+//                 console.log(3);
+//                 setTimeout(() => {
+//                     console.log(4)
+//                 }, ms);
+//             }, ms);
+//         }, ms);
+//     }, ms);
+// }
+// sleep(1000)
+
+//by Promise
+console.log("-------Promise--------");
+function unsleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    })
 }
-
-// Không sử dụng Promise dẫn đến tạo ra callback hell 
-hell(1, function (valueFromA) {
-    hell(valueFromA + 1, function (valueFromB) {
-        hell(valueFromB + 1, function (valueFromC) {
-            hell(valueFromC + 1, function (valueFromD) {
-                console.log(valueFromD + 1);
-            });
-        });
-    });
-});
-
-// Sử dụng Promise sẽ tạo ra đoạn code dễ đọc hơn và vẫn đảm bảo đúng logic
-function notHell(value) {
-    return new Promise(function (resolve) {
-        resolve(value);
-    });
-}
-
-notHell(1)
-    .then(function (value) {
-        return value + 1;
-    })
-    .then(function (value) {
-        return value + 1;
-    })
-    .then(function (value) {
-        return value + 1;
-    })
-    .then(function (value) {
-        console.log(value + 1);
-    });
-
-
-
-
+unsleep(1000)
+        .then(()=> {
+            console.log("1");
+            return unsleep(1000);
+        })
+        .then(()=> {
+            console.log("2");
+            return unsleep(1000);
+        })
+        .then(()=> {
+            console.log("3");
+            return unsleep(1000);
+        })
+        .then(()=> {
+            console.log("4");
+            return unsleep(1000);
+        })
