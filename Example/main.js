95951eafc2f5baea47af99af3976c196237e6c20
itsTranT1706 <khiemtrangia60@gmail.com>
2023-11-02 17:16:43 +0700

    format code

--- Example/main.js
+++ Example/main.js
@@ -9,45 +9,45 @@ function start() {
 start();
 function getCourses(callback) {
     fetch(courseApi)
-    .then(response => response.json())
+        .then(response => response.json())
         .then(callback);
 
 }
 function renderCourses(courses) {
     let listCoursesBlock = document.querySelector("#list-courses");
     let htmls = courses.map((course) => {
-        return ` <li>`+
-        `<h4>${course.name}</h4>`+
-        `<p>${course.description}</p>`+
-        `</li>`
+        return ` <li>` +
+            `<h4>${course.name}</h4>` +
+            `<p>${course.description}</p>` +
+            `</li>`
     });
     console.log(htmls);
     listCoursesBlock.innerHTML = htmls.join("");
 
 }
-function createCourse(data,callback) {
+function createCourse(data, callback) {
     fetch(courseApi, {
         method: "POST", // *GET, POST, PUT, DELETE, etc.
         headers: {
             "Content-Type": "application/json",
             // 'Content-Type': 'application/x-www-form-urlencoded',
-          },
+        },
         body: JSON.stringify(data), // body data type must match "Content-Type" header
-    }) 
-    .then(response => response.json())
-    .then(callback);
-            
+    })
+        .then(response => response.json())
+        .then(callback);
+
 }
 function handleCreateForm() {
-        let createBtn =     document.querySelector("#create");
-        createBtn.onclick = function() {
-                let name = document.querySelector(`input[name="name"]`).value;
-                let  description= document.querySelector(`input[name="Description"]`).value;
-                let formData = {
-                    name: name,
-                    description: description
-                }
-                createCourse(formData,   getCourses(renderCourses));
-                // console.log(name);
+    let createBtn = document.querySelector("#create");
+    createBtn.onclick = function () {
+        let name = document.querySelector(`input[name="name"]`).value;
+        let description = document.querySelector(`input[name="Description"]`).value;
+        let formData = {
+            name: name,
+            description: description
         }
+        createCourse(formData, getCourses(renderCourses));
+        // console.log(name);
     }
+}
